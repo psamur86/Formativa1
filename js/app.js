f8c72fe683c134e3816cf09af1e07ac0d5b824ff
cristobalcrr <cr.carrascor@duocuc.cl>
2023-05-10 01:14:02 -0400

    css y carruseles aun falta javascript

--- js/app.js
+++ js/app.js
@@ -1,5 +1,25 @@
-var Usuario= document.getElementById("Usuario");
-var password= document.getElementById("password");
-error.style.color = "red";
+var usuario = document.getElementsById('Usuario');
+var password = document.getElementsById('passorrd');
+var error = document.getElementsById('error');
+error.style.color = 'red';
 
+function perfilEnter (){
+    console.log('entrando en el perfil');
+
+    var mensajeError = [];
+
+    if(nombre.values === null || nombre.values === ''){
+        mensajeError.push('ingrese su usuario por favor');
+    }
+
+    if(pasword.values === null || pasword.values === ''){
+        mensajeError.push('ingrese su password por favor');
+    }
+
+    error.innerHTML = mensajeError.join(', ');
+
+
+    return false;
+
+}
 
