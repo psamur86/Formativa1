fe68d4d921b5a41d185a44c3af5a390cd88f0344
cristobalcrr <cr.carrascor@duocuc.cl>
2023-05-13 00:38:36 -0400

    todo listo

--- js/app.js
+++ js/app.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
       
       for (var i = 0; i < usuarios.length; i++) {
         if (username === usuarios[i].username && password === usuarios[i].password) {
-          usuarioValido = true;
+          usuarioValido = true ;
           break;
         }
       }
@@ -30,4 +30,34 @@ $(document).ready(function() {
     });
   
   });
+
+
+
+// Conectar al servidor de Socket.io
+const socket = io();
+
+// Manejar el envío de mensajes desde el formulario de chat
+document.querySelector('#chat-input form').addEventListener('submit', (event) => {
+  event.preventDefault();
+  const input = event.target.querySelector('input[type="text"]');
+  const message = input.value;
+  input.value = '';
+  socket.emit('chat-message', message);
+});
+
+// Mostrar los mensajes en la pantalla del chat
+function addMessageToChat(message) {
+  const chatMessages = document.querySelector('#chat-messages');
+  const messageElement = document.createElement('div');
+  messageElement.innerText = message;
+  chatMessages.appendChild(messageElement);
+}
+
+// Manejar la recepción de mensajes desde el servidor
+socket.on('chat-message', (message) => {
+  addMessageToChat(message);
+});
+
+
+<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d26656.56952533336!2d-70.6335647!3d-33.36920739999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2scl!4v1683946869920!5m2!1ses-419!2scl" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
   
