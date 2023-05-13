0c819f80f74a06a46cf67d5da44a94fcffe632f7
cristobalcrr <cr.carrascor@duocuc.cl>
2023-05-13 00:37:04 -0400

    todo listo

--- js/app.js
+++ js/app.js
@@ -1,25 +1,33 @@
-var usuario = document.getElementsById('Usuario');
-var password = document.getElementsById('passorrd');
-var error = document.getElementsById('error');
-error.style.color = 'red';
+$(document).ready(function() {
 
-function perfilEnter (){
-    console.log('entrando en el perfil');
-
-    var mensajeError = [];
-
-    if(nombre.values === null || nombre.values === ''){
-        mensajeError.push('ingrese su usuario por favor');
-    }
-
-    if(pasword.values === null || pasword.values === ''){
-        mensajeError.push('ingrese su password por favor');
-    }
-
-    error.innerHTML = mensajeError.join(', ');
-
-
-    return false;
-
-}
-
+    var usuarios = [
+      { username: "usuario1", password: "contraseña1" },
+      { username: "usuario2", password: "contraseña2" },
+      { username: "usuario3", password: "contraseña3" },
+      { username: "usuario4", password: "contraseña4" },
+      { username: "usuario5", password: "contraseña5" }
+    ];
+  
+    $("#boton-enviar").click(function(event) {
+      event.preventDefault();
+      
+      var username = $("#usuario").val();
+      var password = $("#contraseña").val();
+      var usuarioValido = false;
+      
+      for (var i = 0; i < usuarios.length; i++) {
+        if (username === usuarios[i].username && password === usuarios[i].password) {
+          usuarioValido = true;
+          break;
+        }
+      }
+      
+      if (usuarioValido) {
+        window.location.href = "index.html";
+      } else {
+        $("#mensaje-error").text("Nombre de usuario o contraseña incorrectos.");
+      }
+    });
+  
+  });
+  
